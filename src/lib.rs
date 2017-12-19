4a7ca158798748c483b1bb7c2c6f1e38ba65e9a0
Jure <jure.semrov@outlook.com>
2017-12-19 19:20:34 +0100

    Solution for day 1

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,25 @@ pub mod day1;
 
 #[cfg(test)]
 mod tests {
-    use day1::test;
+    use day1::sum;
 
     #[test]
-    fn it_works() {
-        assert_eq!(test(),5);
-        assert_eq!(2 + 2, 4);
+    fn test_sum1() {
+        assert_eq!(sum("1111"),4);
+    }
+
+    #[test]
+    fn test_sum2() {
+        assert_eq!(sum("1122"),3);
+    }
+
+    #[test]
+    fn test_sum3() {
+        assert_eq!(sum("1234"),0);
+    }
+
+    #[test]
+    fn test_sum4() {
+        assert_eq!(sum("91212129"),9);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+extern crate advent_of_code_2017;
+
+use advent_of_code_2017::day1::*;
+
+
+fn main() {
+  run();
+}
